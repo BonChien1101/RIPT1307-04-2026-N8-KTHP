de709bd39b2ce1e83bb8ca2223dd0acb4ad104ff
DevChienTruong <chientc.b24cc040@stu.ptit.edu.vn>
2026-05-19 02:45:34 +0700

    fix lại build ban đầu, cài đặt môi trường, kết nối DB qua mySQL, seed mẫu

--- backend/.umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from 'D:/BTLCK_NHOM8/backend/node_modules/react-helmet';
---
+++ frontend/src/.umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from 'D:/BTLCK_NHOM8/frontend/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu','getInitialState','initialStateConfig','request',],
+});
+
+export { plugin };
