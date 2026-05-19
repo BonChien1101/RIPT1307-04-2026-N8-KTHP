247a5e972026a263264d3972da5dcab6e7892067
Vinh <vinhpt20106ptit@gmail.com>
2026-05-20 01:50:12 +0700

    Remove RIPT folder and reorganize code into backend and frontend

--- RIPT1307-04-2026-N8-KTHP-main/frontend/src/.umi/plugin-helmet/exports.ts
+++
@@ -1,7 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-<<<<<<< HEAD
-export { Helmet } from 'D:/FINAL WEB/RIPT1307-04-2026-N8-KTHP-main/frontend/node_modules/react-helmet';
-=======
-export { Helmet } from 'D:/BTLCK_NHOM8/frontend/node_modules/react-helmet';
->>>>>>> 609875ffb6e777d3fd382ae675365e4fc0e3792e
--- RIPT1307-04-2026-N8-KTHP-main/frontend/src/access.ts
+++
@@ -1,8 +0,0 @@
-export default function access(initialState: any) {
-  const { user } = initialState || {};
-
-  return {
-    canStudent: user && user.role === 'student',
-    canAdmin: user && user.role === 'admin',
-  };
-}
